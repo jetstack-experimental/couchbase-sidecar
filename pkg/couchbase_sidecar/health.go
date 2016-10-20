7253124997f482f3e2690115eb22f7a5c648d2de
Christian Simon <simon@swine.de>
2016-10-20 10:58:22 +0000

    Adds health check endpoints in sidecar

--- pkg/couchbase_sidecar/health.go
+++ pkg/couchbase_sidecar/health.go
@@ -1,6 +1,10 @@
 package couchbase_sidecar
 
 import (
+	"fmt"
+	"net"
+	"net/http"
+
 	"github.com/Sirupsen/logrus"
 )
 
@@ -12,8 +16,53 @@ func (m *healthCheck) Log() *logrus.Entry {
 	return m.cs.Log().WithField("component", "healthCheck")
 }
 
+func (m *healthCheck) mux() *http.ServeMux {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "couchbase-sidecar - 404 not found")
+	})
+
+	mux.HandleFunc("/_status/ready", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		err := fmt.Errorf("not ready go away")
+		if err == nil {
+			w.WriteHeader(http.StatusOK)
+			m.Log().Debugf("Health check: ok")
+			fmt.Fprint(w, "ok")
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			m.Log().Warnf("Failed health check: %s", err)
+			fmt.Fprint(w, fmt.Sprintf("not ready: %s", err))
+		}
+	})
+
+	return mux
+}
+
 func (m *healthCheck) run() {
-	m.Log().Info("starting")
-	<-m.cs.stopCh
-	m.Log().Info("stopping")
+
+	port := fmt.Sprintf(":%d", 8080)
+
+	// listen on port
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		m.Log().Fatalf("error starting http server on %s: %s", port, err)
+	}
+
+	mux := m.mux()
+
+	m.Log().Infof("server listening on http://%s/", port)
+
+	// handle stop signal
+	go func() {
+		<-m.cs.stopCh
+		m.Log().Infof("stopping server listening on http://%s/", port)
+		listener.Close()
+	}()
+
+	http.Serve(listener, mux)
 }
